ddf2b72a1f3674a32712352783e60eb062384e98
md <marceldiomare@gmail.com>
2024-06-14 19:46:23 +0300

    added get_all_vacancies

--- DBManager.py
+++ DBManager.py
@@ -16,3 +16,18 @@ class DBManager:
         """
         self.cur.execute(f"SELECT company_name, open_vacancies FROM employers")
         return self.cur.fetchall()
+
+
+    def get_all_vacancies(self):
+        """
+        Получает список всех вакансий с информацией о компании, названии вакансии, зарплате и ссылке.
+
+        :return: List of tuples with company name, vacancy name, salary, and vacancy URL.
+        """
+        self.cur.execute(f"select employers.company_name, vacancies.vacancy_name, vacancies.salary_from, "
+                         f"vacancies.vacancy_url from vacancies join employers using(employer_id)")
+        return self.cur.fetchall()
+
+
+
+
