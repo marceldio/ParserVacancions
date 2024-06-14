e48a42e6e531468b34ac4469fd08c90fb0fceccf
md <marceldiomare@gmail.com>
2024-06-14 19:36:28 +0300

    added create_base

--- utils.py
+++ utils.py
@@ -29,3 +29,50 @@ def fetch_hh_ru_data(company_list: List[str]) -> List[Dict[str, Any]]:
             'vacancies': vacancies
         })
     return data
+
+
+def create_base(base_name: str, params: dict):
+    """Функция для создания базы данных и таблиц для сохранения данных
+
+    Args:
+        имя_базы_данных (str): Имя базы данных, которую необходимо создать
+    Создает базу данных с указанным именем, а также две таблицы 'employers' и 'vacancies' для хранения информации
+    о работодателях и вакансиях соответственно.
+    """
+
+    # conn = psycopg2.connect(**params)
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+    try:
+        cur.execute(f"DROP DATABASE IF EXISTS {base_name}")
+    except:
+        cur.execute(f"CREATE DATABASE {base_name}")
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=base_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE employers (
+                employer_id SERIAL PRIMARY KEY,
+                company_name VARCHAR(500) NOT NULL,
+                open_vacancies INTEGER,
+                employer_url TEXT,
+                description TEXT)
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancies (
+                vacancy_id SERIAL PRIMARY KEY,
+                employer_id INT REFERENCES employers(employer_id),
+                vacancy_name VARCHAR(500) NOT NULL,
+                salary_from INTEGER,
+                vacancy_url TEXT)
+        """)
+
+    conn.commit()
+    conn.close()
+
